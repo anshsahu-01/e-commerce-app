d443898be70dfb0a5cf552e7595380c82a13b911
anshsahu-01 <anshsahu178@gmail.com>
2026-05-10 21:46:16 +0530

    Wishlist And Profile Screen UI Completed

--- client/app/(tabs)/favourites.tsx
+++ client/app/(tabs)/favourites.tsx
@@ -1,10 +1,41 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useWishlist } from '@/context/WishlistContext'
+import { useRouter } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '@/components/Header'
+import { ScrollView } from 'react-native-gesture-handler'
+import { TouchableOpacity } from 'react-native'
+import ProductCard from '@/components/ProductCard'
 
 export default function Favourites() {
+
+  const {wishlist} = useWishlist()
+  const router = useRouter()
+
   return (
-    <View>
-      <Text>Favourites</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      <Header title='Wishlist' showCart showMenu/>
+
+      {
+      wishlist.length > 0 ? (
+        <ScrollView className='flex-1 px-4 mt-4' showsVerticalScrollIndicator={false}>
+          <View className='flex-row justify-between flex-wrap'>
+            {wishlist.map((product) => (
+              <ProductCard key={product._id} product={product}/>
+            ))}
+          </View>
+        </ScrollView>
+      ) : (
+        <View className='flex-1 items-center justify-center'>
+          <Text className='text-lg text-secondary'>Your wishlist is empty</Text>
+          <TouchableOpacity 
+          className='mt-4 bg-primary px-6 py-3 rounded-full' 
+          onPress={()=>router.push('/')}> 
+            <Text className='text-white font-bold text-xl'>Shop Now !!</Text>
+          </TouchableOpacity>
+          </View>
+      )}
+    </SafeAreaView>
   )
 }
